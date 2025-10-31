014b179f9dc0f192068c7c4cfb5be5c0ad138d20
Deniss <cvetkovsdeniss4@gmail.com>
2025-10-31 10:23:41 +0200

    Dome function creation excercise in part 2

---
+++ part_2/function.js
@@ -0,0 +1,14 @@
+var num1 = 3;
+var num2 = 7;
+
+function findMax(n1,n2){
+    if(n1 > n2){
+        return n1;
+    }else{
+        return n2;
+    }
+    return 0;
+}
+
+console.log(findMax(num1,num2));
+console.log(num1 + num2);
