e88c27d9062e71ecc1eb12948244ca4d511147aa
Deniss <cvetkovsdeniss4@gmail.com>
2025-10-31 09:50:22 +0200

    Done geometry excercise in part 2

---
+++ part_2/math.js
@@ -0,0 +1,8 @@
+var r = 5;
+
+let s = (Math.PI * r^2).toFixed(2);
+let c = (Math.PI * r * 2).toFixed(2);
+
+console.log("Radiuss = " + r)
+console.log("Laukums = " + s);
+console.log("Riņķa linijas garums = " + c);
